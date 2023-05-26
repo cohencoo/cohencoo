5eda1e36825dbc9949aa3cbce6034bcc1aaffedb
cohencoo <cohencoombs@outlook.com>
2023-05-26 22:07:28 +0930

    Detachment of business from cohencoo

--- README.md
+++ README.md
@@ -1,5 +1,9 @@
 # Hi, I’m Cohen.
 
-> I’m a passionate full-stack developer. I’ve been creating websites out of personal interest for over 6 years, which started with the basics of HTML, gradually leading into the freelance industry with international clients. More recently, I’ve been progressive to gain more experience in the back-end/data industry, fused with the development of Artificial Intelligence/Machine learning using NodeJS, creating chatbots, and a smaller scale version of GPT3 by OpenAI. In the physical world, I enjoy creating practical products. I have profound interest with Arduino, home automation with Wifi controllers (ESP32), a custom touchscreen phone written in C++, and most recently; a robot rover dog, featuring 8 control joints and ultrasonic sensors, inspired by Boston Dynamics; a company, whom inspires my passion & interest for robotics.
+> I’m a passionate full-stack developer.
 
-> Additionally, I’m an enthusiast of 3D printing for practical use cases. Complimenting the ability to 3D print based on a 3D design in Fusion360, which is able to solve an extensive array of problems in the household, or projects - which can be quickly turned into professional grade quality.
+I’ve been creating websites out of personal interest for over 6 years, which started with the basics of HTML, gradually leading into the freelance industry with international clients. In 2023 as a side-hustle, I registered my own business; [Bespoke Web Dev](https://bespokewebdev.com), managing website development on an increasingly larger scale.
+
+I’ve been progressive to gain more experience in backend development, fused with AI, ML using NodeJS, creating chat-bots and AI solutions for software. In the physical world, I enjoy creating practical products. Using Arduino, I've created home automated devices with WiFi compatibility (using ESP32), a custom touchscreen phone written in C++, and most recently; a robot rover dog, featuring 8 control joints and ultrasonic sensors, inspired by Boston Dynamics.
+
+Additionally, I’m grateful for the skill to 3D print for practical use cases, such as household solutions and product prototyping through the use of Fusion CAD modelling.
--- assets/main.js
+++ assets/main.js
@@ -1,27 +1,30 @@
-const nav = document.querySelector('nav')
-const footer = document.querySelector('footer')
+const nav = document.querySelector("nav")
+const footer = document.querySelector("footer")
 const carousel = document.querySelector(".carousel")
 const currentYear = new Date().getFullYear()
-const isHome = () => window.location.pathname == '/' || window.location.pathname == '/index.html'
+const isHome = () => window.location.pathname == "/" || window.location.pathname == "/index.html"
 
 function contact() {
-    Menu.open({
-        title: 'Contact links',
-        content: `
-        Contact me via linkedin or email.
-        <br><br>
+    Menu.open(
+        {
+            title: "Contact me via:",
+            content: `
+        <br>
         <svg xmlns="http://www.w3.org/2000/svg" style="margin-right: 1rem; width: 1.2rem; height: 1.2rem; vertical-align: middle" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><path d="M16 8a6 6 0 0 1 6 6v7h-4v-7a2 2 0 0 0-2-2 2 2 0 0 0-2 2v7h-4v-7a6 6 0 0 1 6-6z"></path><rect x="2" y="9" width="4" height="12"></rect><circle cx="4" cy="4" r="2"></circle></svg>
         <a style="font-size: 1.1rem; font-weight: 500;" href="https://www.linkedin.com/in/cohencoombs/" target="_blank"> www.linkedin.com/in/cohencoombs</a>
         <br><br>
         <svg xmlns="http://www.w3.org/2000/svg" style="margin-right: 1rem; width: 1.2rem; height: 1.2rem; vertical-align: middle" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><circle cx="12" cy="12" r="4"></circle><path d="M16 8v5a3 3 0 0 0 6 0v-1a10 10 0 1 0-3.92 7.94"></path></svg>
-        <a style="font-size: 1.1rem; font-weight: 500;" href="mailto:cohencoombs@outlook.com"> cohencoombs@outlook.com </a>`
-    }, true)
+        <a style="font-size: 1.1rem; font-weight: 500;" href="mailto:cohencoombs@outlook.com"> cohencoombs@outlook.com </a>`,
+        },
+        true
+    )
 }
 
 function openMenu() {
-    Menu.open({
-        title: 'Menu',
-        content: `
+    Menu.open(
+        {
+            title: "Menu",
+            content: `
         
         <a href="/#about">About</a>
         <br><br>
@@ -33,10 +36,12 @@ function openMenu() {
         <a style="font-size: 1.1rem; font-weight: 500;" href="mailto:cohencoombs@outlook.com"> cohencoombs@outlook.com </a>
 
         <br><br>
-        <p>Freelance Services</p>
-        <a href="/services/">See Freelance Services</a>
-        `
-    }, false)
+        <p>Web Design Services</p>
+        <a href="https://bespokewebdev.com">See Services</a>
+        `,
+        },
+        false
+    )
 }
 
 nav.innerHTML = `
@@ -46,15 +51,14 @@ nav.innerHTML = `
     <li><a href="/projects/">Projects</a></li>
     <li onclick="contact()"><a href="#">Contact</a></li>
     <li>
-        <a href="/services/">
-            <button id="services-cta" style="margin: 0; font-size: 0.9rem; padding: 0rem 1rem !important; opacity: 0.5; background: var(--theme); border-radius: 100rem; font-weight: 600">See Freelance Services</button>
+        <a href="https://bespokewebdev.com">
+            <button id="services-cta" style="margin: 0; font-size: 0.9rem; padding: 0rem 1rem !important; opacity: 0.5; background: var(--theme); border-radius: 100rem; font-weight: 600">Web Design Services</button>
         </a>
     </li>
 </ul>
 <div onclick="openMenu()" class="mobile-menu">
     <svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><line x1="3" y1="12" x2="21" y2="12"></line><line x1="3" y1="6" x2="21" y2="6"></line><line x1="3" y1="18" x2="21" y2="18"></line></svg>
 </div>`
-
 
 footer.innerHTML = `
     <h2>Contact</h2>
@@ -80,150 +84,154 @@ footer.innerHTML = `
     <h2>Links</h2>
     <div class="link-container">
         <a class="link" target="_blank" href="../projects/">Projects</a>
-        <a class="link" target="_blank" href="../services/">Services</a>
+        <a class="link" target="_blank" href="https://bespokewebdev.com">Web Design Services</a>
     </div>
     <br>
     <p style="color: var(--subtext)"> © Cohen Coombs ${currentYear}. All Rights Reserved </p>
 `
 
-if (isHome()) for (i = 0; i < 5; i++)
-    document.getElementById("rating-section").innerHTML += `<svg xmlns="http://www.w3.org/2000/svg" style="margin-right: 0.3rem; width: 1.3rem; height: 1.3rem; color: #ffc800;" viewBox="0 0 24 24" fill="#ffc800" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><polygon points="12 2 15.09 8.26 22 9.27 17 14.14 18.18 21.02 12 17.77 5.82 21.02 7 14.14 2 9.27 8.91 8.26 12 2"></polygon></svg>`
+if (isHome())
+    for (i = 0; i < 5; i++)
+        document.getElementById(
+            "rating-section"
+        ).innerHTML += `<svg xmlns="http://www.w3.org/2000/svg" style="margin-right: 0.3rem; width: 1.3rem; height: 1.3rem; color: #ffc800;" viewBox="0 0 24 24" fill="#ffc800" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><polygon points="12 2 15.09 8.26 22 9.27 17 14.14 18.18 21.02 12 17.77 5.82 21.02 7 14.14 2 9.27 8.91 8.26 12 2"></polygon></svg>`
 
 let variation = 0
-let inc = 0.04;
-let scl = 40;
-let cols, rows, flow;
-let offset = 0;
-let particles = [];
+let inc = 0.04
+let scl = 40
+let cols, rows, flow
+let offset = 0
+let particles = []
 const banner = "canvas"
 
-function windowResized() { resizeCanvas(windowWidth, windowHeight) }
+function windowResized() {
+    resizeCanvas(windowWidth, windowHeight)
+}
 
 function setup() {
-    particles = [];
-    createCanvas(windowWidth, windowHeight).parent(banner);
-    colorMode(HSB, 360);
-    cols = floor(width / scl);
-    rows = floor(height / scl);
-    flow = new Array(cols * rows);
-    for (let i = 0; i < 3000; i++) particles[i] = new Particle(); // set to 900000
+    particles = []
+    createCanvas(windowWidth, windowHeight).parent(banner)
+    colorMode(HSB, 360)
+    cols = floor(width / scl)
+    rows = floor(height / scl)
+    flow = new Array(cols * rows)
+    for (let i = 0; i < 3000; i++) particles[i] = new Particle() // set to 900000
 }
 
 function draw() {
-    let y_offset = 0;
+    let y_offset = 0
     for (let y = 0; y < rows; y++) {
-        let x_offset = 0;
+        let x_offset = 0
         for (let x = 0; x < cols; x++) {
-            let index = x + y * cols;
-            let angle = noise(x_offset, y_offset, offset) * TWO_PI * 4;
-            let v = p5.Vector.fromAngle(angle);
-            v.setMag(3);
-            flow[index] = v;
-            x_offset += inc;
+            let index = x + y * cols
+            let angle = noise(x_offset, y_offset, offset) * TWO_PI * 4
+            let v = p5.Vector.fromAngle(angle)
+            v.setMag(3)
+            flow[index] = v
+            x_offset += inc
             // rotate(v.heading());
         }
-        y_offset += inc;
-        offset += 0.0001;
+        y_offset += inc
+        offset += 0.0001
     }
     for (let i = 0; i < particles.length; i++) {
-        particles[i].follow(flow);
-        particles[i].update();
-        particles[i].edges();
-        particles[i].show();
+        particles[i].follow(flow)
+        particles[i].update()
+        particles[i].edges()
+        particles[i].show()
     }
 }
 
-let minRange = 225;
-let maxRange = 270;
+let minRange = 225
+let maxRange = 270
 
 function Particle() {
-    this.pos = createVector(random(width), random(height));
-    this.vel = createVector(0, 0);
-    this.acc = createVector(0, 0);
-    this.maxspeed = 11; // default 8
-    this.alternate = false;
+    this.pos = createVector(random(width), random(height))
+    this.vel = createVector(0, 0)
+    this.acc = createVector(0, 0)
+    this.maxspeed = 11 // default 8
+    this.alternate = false
 
-    this.prevPos = this.pos.copy();
+    this.prevPos = this.pos.copy()
 
-    this.h = minRange;
-    this.colour = color(0, 0, 0, 20);
+    this.h = minRange
+    this.colour = color(0, 0, 0, 20)
 
     this.update = function () {
-        this.vel.add(this.acc);
-        this.vel.limit(this.maxspeed);
-        this.pos.add(this.vel);
-        this.acc.mult(0);
+        this.vel.add(this.acc)
+        this.vel.limit(this.maxspeed)
+        this.pos.add(this.vel)
+        this.acc.mult(0)
 
         if (this.alternate) {
-            this.h += 0.5;
-            if (this.h > maxRange) this.alternate = false;
+            this.h += 0.5
+            if (this.h > maxRange) this.alternate = false
         } else {
-            this.h -= 0.5;
-            if (this.h < minRange) this.alternate = true;
+            this.h -= 0.5
+            if (this.h < minRange) this.alternate = true
         }
 
-        this.colour = color(this.h, 360, 360, 8);
-    };
+        this.colour = color(this.h, 360, 360, 8)
+    }
 
     this.follow = function (vectors) {
-        let x = floor(this.pos.x / scl);
-        let y = floor(this.pos.y / scl);
-        let index = x + y * cols;
-        let force = vectors[index];
-        this.applyForce(force);
-    };
+        let x = floor(this.pos.x / scl)
+        let y = floor(this.pos.y / scl)
+        let index = x + y * cols
+        let force = vectors[index]
+        this.applyForce(force)
+    }
 
     this.applyForce = (force) => this.acc.add(force)
 
     this.show = function () {
-        stroke(this.colour);
-        strokeWeight(0.5);
-        line(this.pos.x, this.pos.y, this.prevPos.x, this.prevPos.y);
-        this.updatePrev();
-    };
+        stroke(this.colour)
+        strokeWeight(0.5)
+        line(this.pos.x, this.pos.y, this.prevPos.x, this.prevPos.y)
+        this.updatePrev()
+    }
 
     this.updatePrev = function () {
         this.prevPos.x = this.pos.x
         this.prevPos.y = this.pos.y
-    };
+    }
 
     this.edges = function () {
         if (this.pos.x > width) {
-            this.pos.x = 0;
-            this.updatePrev();
+            this.pos.x = 0
+            this.updatePrev()
         }
         if (this.pos.x < 0) {
-            this.pos.x = width;
-            this.updatePrev();
+            this.pos.x = width
+            this.updatePrev()
         }
         if (this.pos.y > height) {
-            this.pos.y = 0;
-            this.updatePrev();
+            this.pos.y = 0
+            this.updatePrev()
         }
         if (this.pos.y < 0) {
-            this.pos.y = height;
-            this.updatePrev();
+            this.pos.y = height
+            this.updatePrev()
         }
-    };
+    }
 }
 
-
 async function softFadeOpacity(element, duration, targetOpacity) {
-    const start = performance.now();
-    const startOpacity = parseFloat(window.getComputedStyle(element).opacity);
-    const delta = targetOpacity - startOpacity;
+    const start = performance.now()
+    const startOpacity = parseFloat(window.getComputedStyle(element).opacity)
+    const delta = targetOpacity - startOpacity
     return new Promise((resolve) => {
         function step(timestamp) {
-            const progress = (timestamp - start) / duration;
-            element.style.opacity = startOpacity + delta * progress;
+            const progress = (timestamp - start) / duration
+            element.style.opacity = startOpacity + delta * progress
             if (progress < 1) {
-                window.requestAnimationFrame(step);
+                window.requestAnimationFrame(step)
             } else {
-                resolve();
+                resolve()
             }
         }
-        window.requestAnimationFrame(step);
-    });
+        window.requestAnimationFrame(step)
+    })
 }
 
 setInterval(async () => {
@@ -232,20 +240,19 @@ setInterval(async () => {
         await softFadeOpacity(banner, 500, 0)
         variation++
         if (variation % 2 == 0) {
-            minRange = 225;
-            maxRange = 270;
+            minRange = 225
+            maxRange = 270
         } else {
-            minRange = 5;
-            maxRange = 80;
+            minRange = 5
+            maxRange = 80
         }
-        setup();
+        setup()
         await softFadeOpacity(banner, 500, 1)
     }
-}, 6000);
+}, 6000)
 
 document.addEventListener("scroll", () => {
-    const scroll = window.scrollY;
-    const banner = document.querySelector(".banner");
+    const scroll = window.scrollY
 
     if (scroll > 10) {
         nav.classList = "nav-active"
@@ -254,15 +261,10 @@ document.addEventListener("scroll", () => {
         nav.classList = "nav-inactive"
         document.querySelector("#services-cta").style.opacity = "0.5"
     }
-
-    if (isHome() && window.innerWidth > 1000) {
-        document.getElementById("canvas").style.scale = 1 + scroll / 2000;
-        banner.style.scale = 1 - scroll / 4000
-        banner.style.boxShadow = `inset 0px 0px ${scroll / 1.5 }px 0px rgba(255,255,255,0.02)`;
-    }
 })
 
-let items = [{
+let items = [
+    {
         name: "HTML",
         icon: "html.png",
     },
@@ -315,7 +317,7 @@ function nextRender() {
     })
 
     carousel.appendChild(group)
-};
+}
 
 if (isHome()) for (let i = 0; i < 6; i++) nextRender()
 
